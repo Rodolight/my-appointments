809b538d99e5cf2ced7547a67420bc6d0b5e0284
Rodolfo De Pena <rde_pena@hotmail.com>
2021-04-23 00:19:00 -0400

    Update migrations

--- database/migrations/2021_04_23_040408_add_device_token_to__users.php
+++ database/migrations/2021_04_23_040408_add_device_token_to__users.php
@@ -13,7 +13,7 @@ class AddDeviceTokenToUsers extends Migration
      */
     public function up()
     {
-        Schema::table('_users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('device_token')->nullable();
         });
     }
@@ -25,7 +25,7 @@ class AddDeviceTokenToUsers extends Migration
      */
     public function down()
     {
-        Schema::table('_users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('device_token');
         });
     }
